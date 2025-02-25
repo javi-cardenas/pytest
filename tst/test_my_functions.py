5c2ba68add486606631d67242007847cecb023a9
Javi Cardenas <javicard@pm.me>
2025-02-25 10:57:49 -0500

    used class and fixture based tests

--- tst/test_my_functions.py
+++ tst/test_my_functions.py
@@ -1,6 +1,19 @@
 import pytest
-
 import src.my_functions as my_functions
 
-def test_add():
-    pass
+class TestMyFunctions():
+    def test_add(self):
+        result = my_functions.add(1, 4)
+        assert result == 5
+
+    def test_add_strings(self):
+        result = my_functions.add("i like ", "burgers")
+        assert result == "i like burgers"
+
+    def test_divide(self):
+        result = my_functions.divide(10, 5)
+        assert result == 2
+
+    def test_divide_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            my_functions.divide(10, 0)
